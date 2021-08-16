f3b1dd05e3e8202a0da562fa2b3dd232fdf4cd46
Léo Belini <leobelini96@gmail.com>
2021-08-16 19:09:13 -0300

    feat: add helmet

--- src/app/express.ts
+++ src/app/express.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
 import { envs } from './envs'
 
 class ExpressApp {
@@ -14,19 +15,19 @@ class ExpressApp {
     })
 
     this.express.post('/', (req, res) => {
-      console.log({body:req.body})
+      console.log({ body: req.body })
       res.send(JSON.stringify(req.body, null, 2))
     })
   }
 
-  private configuration(){
+  private configuration() {
     this.express.use(cors())
-    this.express.use(express.urlencoded({extended:false}))
+    this.express.use(express.urlencoded({ extended: false }))
     this.express.use(express.json())
+    this.express.use(helmet())
   }
 
-  public start() {    
-
+  public start() {
     this.express.listen(envs.PORT_APP, () => {
       console.log(`Example app listening at http://localhost:${envs.PORT_APP}`)
     })
